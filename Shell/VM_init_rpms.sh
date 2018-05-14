6fd2b5db2dab53694e8634ff87f710c606511484
tinderbox <abc1238364@gmail.com>
2018-05-14 17:56:33 +0800

    Create VM_init_rpms.sh

---
+++ Shell/VM_init_rpms.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+# Basic
+yum install nc
+yum install curl
+yum install wget
+
+# For Pythons
+yum install python-pip
+pip install --upgrade pip
+pip install elasticsearch
+
+
+# For ELK
+yum install java
+
+
+# ELK 5.6.4
+rpm -ivh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.4.rpm
+rpm -ivh https://artifacts.elastic.co/downloads/logstash/logstash-5.6.4.rpm
+rpm -ivh https://artifacts.elastic.co/downloads/kibana/kibana-5.6.4-x86_64.rpm
+
+
+#####
+# ELK 6.x
+#cat >/etc/yum.repos.d/elasticsearch.repo <<EOL
+#[elasticsearch-6.x]
+#name=Elasticsearch repository for 6.x packages
+#baseurl=https://artifacts.elastic.co/packages/6.x/yum
+#gpgcheck=1
+#gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+#enabled=1
+#autorefresh=1
+#type=rpm-md
+#EOL
+#####
