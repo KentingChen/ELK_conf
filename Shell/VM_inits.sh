a885e767f42f948edbd072c1169929128e33ca2f
tinderbox <abc1238364@gmail.com>
2018-05-14 17:59:15 +0800

    Update and rename VM_init_rpms.sh to VM_inits.sh

---
+++ Shell/VM_inits.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## YUM 
+# Basic
+yum install nc
+yum install curl
+yum install wget
+
+# For Pythons
+yum install python-pip
+pip install --upgrade pip
+pip install elasticsearch
+
+
+# Settings
+timedatectl set-timezone "Asia/Taipei"
