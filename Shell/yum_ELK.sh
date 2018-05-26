048a2a434c09401a7647bcca94175dac44dedafb
tinderbox <abc1238364@gmail.com>
2018-05-26 12:58:33 +0800

    Update yum_ELK.sh

--- Shell/yum_ELK.sh
+++ Shell/yum_ELK.sh
@@ -18,20 +18,20 @@ rpm -ivh https://artifacts.elastic.co/downloads/kibana/kibana-$ELK_version-x86_6
 
 
 ##### ELK 6.x
-#cat >/etc/yum.repos.d/elasticsearch.repo <<EOL
-#[elasticsearch-6.x]
-#name=Elasticsearch repository for 6.x packages
-#baseurl=https://artifacts.elastic.co/packages/6.x/yum
-#gpgcheck=1
-#gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-#enabled=1
-#autorefresh=1
-#type=rpm-md
-#EOL
+cat >/etc/yum.repos.d/elasticsearch.repo <<EOL
+[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOL
 
-# yum -y install elasticsearch
-# yum -y install logstash
-# yum -y install kibana
+ yum -y install elasticsearch
+ yum -y install logstash
+ yum -y install kibana
 #####
 
 
